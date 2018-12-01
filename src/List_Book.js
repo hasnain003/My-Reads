c64541388221c8a75a40b09f807b1154998d8efb
hasnain003 <mdhasnain839@gmail.com>
2018-12-01 18:22:43 +0530

    Initialize List_Book

---
+++ src/List_Book.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+import {Link} from 'react-router-dom'
+import BookShelf from './Book_Shelf.js'
+
+class ListBook extends Component {
+
+	render() {
+		return ()
+	}
+}
