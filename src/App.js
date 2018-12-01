9e4d1b7c280e0b32f1277d9a4ba420f688e41f0d
hasnain003 <mdhasnain839@gmail.com>
2018-12-01 14:53:52 +0530

    Add Bookshelf

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import Book from './Book.js'
+import BookShelf from './Book_Shelf.js'
 
 class BooksApp extends React.Component {
   state = {
@@ -16,11 +17,25 @@ class BooksApp extends React.Component {
 render() {
     console.log(this.state.Books)
     return (
-      <div>
-        <ol className="books-grid">
-          {this.state.Books.length > 0 && 
-          this.state.Books.map((book) => (<Book book={book}/>))}
-        </ol>
+      <div className="app">
+        <div className="list-books">
+          <div className="list-books-title">
+            <h1>MyReads</h1>
+          </div>
+          <div className="list-books-content">
+            <div>
+              <BookShelf books={this.state.Books.filter((book) => (book.shelf === "currentlyReading"))} 
+              title="Currently Reading" 
+              onChangeShelf={this.props.onChange}/>
+              <BookShelf books={this.state.Books.filter((book) => (book.shelf === "Read"))} 
+              title="Read" 
+              onChangeShelf={this.props.onChange}/>
+              <BookShelf books={this.state.Books.filter((book) => (book.shelf === "Want to Read"))} 
+              title="Want to Read" 
+              onChangeShelf={this.props.onChange}/>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
