61403d3d93a7c2ab542c1844557af97c2d3b5ab9
hasnain003 <mdhasnain839@gmail.com>
2018-12-01 15:19:17 +0530

    Validate datatypes

--- src/Book_Shelf.js
+++ src/Book_Shelf.js
@@ -1,2 +1,17 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
+
+class BookShelf extends Component {
+
+	static propTypes = {
+		books: PropTypes.array.isRequired,
+		title: PropTypes.string.isRequired,
+		onChangeShelf:PropTypes.func.isRequired
+	}
+
+render() {
+
+}
+}
+
+export default BookShelf;
