dd639b4a9cde20a3381b621e4c681dd95039053e
hasnain003 <mdhasnain839@gmail.com>
2018-12-01 22:40:33 +0530

    Add three shelf

--- src/List_Book.js
+++ src/List_Book.js
@@ -3,9 +3,41 @@ import PropTypes from 'prop-types'
 import {Link} from 'react-router-dom'
 import BookShelf from './Book_Shelf.js'
 
+
+
 class ListBook extends Component {
 
+	static propTypes = {
+		books : PropTypes.array.isRequired,
+		update : PropTypes.func.isRequired
+	}
+
+
 	render() {
-		return ()
+		const books=this.props.books;
+		const update=this.props.update;
+		return (
+			<div className="list-books">
+          		<div className="list-books-title">
+            		<h1>MyReads</h1>
+          		</div>
+          		<div className="list-books-content">
+            		<div>
+              			<BookShelf books={books.filter((book) => (book.shelf === "currentlyReading"))} 
+              			title="Currently Reading" 
+             			onChangeShelf={update}/>
+              			<BookShelf books={books.filter((book) => (book.shelf === "read"))} 
+              			title="Read" 
+              			onChangeShelf={update}/>
+              			<BookShelf books={books.filter((book) => (book.shelf === "wantToRead"))} 
+              			title="Want to Read" 
+              			onChangeShelf={update}/>
+            		</div>
+          		</div>
+        	</div>
+        	
+		)
 	}
 }
+
+export default ListBook;
