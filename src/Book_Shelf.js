76ec35c995f551406bec9d46007d30635db58d53
hasnain003 <mdhasnain839@gmail.com>
2018-12-02 14:14:15 +0530

    Add Destructuring Assignment

--- src/Book_Shelf.js
+++ src/Book_Shelf.js
@@ -10,20 +10,19 @@ class BookShelf extends Component {
 		onChangeShelf:PropTypes.func.isRequired
 	}
 
-	update_book = (book, shelf) => {
+	updateBook = (book, shelf) => {
     this.props.onChangeShelf(book, shelf)
   }
 
 	render() {
-		const books=this.props.books;
-		const title=this.props.title;
+		const { books,title } = this.props;
 		return (
 			<div className="bookshelf">
                 <h2 className="bookshelf-title">{title}</h2>
                 <div className="bookshelf-books">
                 	<ol className="books-grid">
                     	{books.map((book,index) => (<Book book={book} key={index} onUpdate={(shelf) => {
-                    		this.update_book(book,shelf)
+                    		this.updateBook(book,shelf)
                     	}}/> ))}
                     </ol>
                 </div>
