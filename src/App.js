893f087394f6577efa03631cbe4e4deab5a8af31
hasnain003 <mdhasnain839@gmail.com>
2018-12-02 13:31:42 +0530

    Remove typos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
-import * as BooksAPI from './BooksAPI'
-import './App.css'
 import ListBook from './List_Book'
 import BookSearch from './Book_Search'
+import * as BooksAPI from './BooksAPI'
+import './App.css'
 
 class BooksApp extends React.Component {
   state = {
@@ -16,22 +16,31 @@ class BooksApp extends React.Component {
     })
   }
 
-  update_book_details = (book,shelf) => {
+  updateBookDetails = (book,shelf) => {
     BooksAPI.update(book,shelf).then(() => {
       BooksAPI.getAll().then((Books) => {
       this.setState({ Books })
       })
     })
   }
+
 render() {
     console.log(this.state.Books)
     return (
       <div className="app">
-        <Route exact path="/" 
-        render={() => (<ListBook books={this.state.Books}
-         update={this.update_books_details}/>)}/>
-        <Route exact path='/search' render={() => (<BookSearch books={this.state.Books}
-          update={this.update_book_details}/>)}/>
+          <Route exact path='/' 
+          render={() => (
+            <ListBook 
+            books={this.state.Books}
+            update={this.updateBookDetails}
+           />
+           )}/>
+          <Route exact path='/search' render={() => (
+            <BookSearch 
+            books={this.state.Books}
+            update={this.updateBookDetails}
+            />
+            )}/>
       </div>
     ) 
   }
