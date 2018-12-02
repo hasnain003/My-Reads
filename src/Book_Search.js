1eb136f785680c82391525dc8a798b24c59297b6
hasnain003 <mdhasnain839@gmail.com>
2018-12-02 14:19:26 +0530

    Add state

--- src/Book_Search.js
+++ src/Book_Search.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 
 class BookSearch extends Component {
+	state = {
+		Books: [],
+		query: ''
+	}
+
 	static propTypes = {
 		books : PropTypes.array.isRequired,
 		update: PropTypes.func.isRequired
 	}
+
 	render() {
 		return(
 			<div className="search-books">
@@ -13,7 +19,7 @@ class BookSearch extends Component {
               <button className="close-search" 
               onClick={() => this.setState({ showSearchPage: false })}>Close</button>
               <div className="search-books-input-wrapper">
-                <input type="text" placeholder="Search by title or author"/>
+                <input type="text" placeholder="Search by title or author" value={this.state.query} onChange={this.handleChange}/>
               </div>
             </div>
             <div className="search-books-results">
